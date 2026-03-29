096a3d22e633b04563b1cde4242dd7f7ecd211ae
Priyanshi <priyanshi25xd@gmail.com>
2026-03-29 13:36:41 +0530

    testing

--- scraper/pubmed_scraper.py
+++ scraper/pubmed_scraper.py
@@ -2,6 +2,8 @@ from Bio import Entrez
 from dotenv import load_dotenv
 import os
 from utils.chunking import chunking
+from utils.tagging import tagging
+from scoring.trust_score import TrustScore
 
 load_dotenv()
 email = os.getenv("email")
@@ -20,12 +22,15 @@ records = Entrez.read(response)
 article = records['PubmedArticle'][0]['MedlineCitation']['Article']
 
 authors_list = article['AuthorList']
-authors = []
+
+authors = [] 
+affiliations = []
 
 for author in authors_list:
     first = author['ForeName']
     last = author['LastName']
     authors.append(f"{first} {last}")
+    affiliations.append(author['AffiliationInfo'][0]['Affiliation'].split(',')[1])
 
 author = ", ".join(authors) 
 
@@ -39,7 +44,7 @@ published_date = f"{year}-{month}-{day}"
 abstract = str(article['Abstract']['AbstractText'][0])
 content_chunks = chunking(abstract)
 
-language = str(article["Language"][0])
+language = str(article.get("Language"[0], "Unknown"))
 
 try:
     region = records['PubmedArticle'][0]['MedlineCitation']['Country']
@@ -47,18 +52,28 @@ except:
     region = "Unknown"
 
 
-keywords = records['PubmedArticle'][0]['MedlineCitation']['KeywordList']
-topic_tags = [str(k) for k in keywords[0]]
+try:
+    keywords = records['PubmedArticle'][0]['MedlineCitation']['KeywordList']
+    topic_tags = [str(k) for k in keywords[0]]
+except:
+    topic_tags = tagging(abstract)
 
 
-print({ 
+data = { 
      "source_url": f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/", 
      "source_type": "pubmed", 
      "author": author, 
+     "affiliations" : affiliations,
      "published_date": published_date, 
      "language": language, 
      "region": region, 
      "topic_tags": topic_tags, 
      "trust_score": "", 
      "content_chunks": content_chunks
-     } )
+     }
+
+# print(data)
+
+score = TrustScore(data)
+a_score = score.author_credibility()
+print(a_score)
